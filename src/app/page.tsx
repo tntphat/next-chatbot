6f2a1303d979df234624cb599e77054103c1ac0c
phat <phat.tnt@teko.vn>
2025-07-29 17:44:53 +0700

    4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image';
 import styles from './page.module.css';
 import ChatbotButton from '@/components/ChatbotButton';
+import FloatingChatbot from 'teko-chatbot-ui/dist/index.mjs';
+import 'teko-chatbot-ui/dist/index.css';
 
 export default function Home() {
   return (
-    <div className={styles.page}>Hello world 3{false && <ChatbotButton />}</div>
+    <div className={styles.page}>
+      Hello world 4 {typeof window !== undefined && <FloatingChatbot />}
+    </div>
   );
 }
